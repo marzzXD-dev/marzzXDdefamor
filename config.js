41f2f8edc9b33759dd3ee0cf7da66a6be68e567a
marzzXD-dev <beryymarzz@gmail.com>
2025-12-14 14:37:56 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+//credit By Limzzzhama 
+//no hapus credit ketauan hapus hitam//
+
+module.exports = {
+  domain: "https://marzcuy.sagitarius.web.id", // Ubah jadi Domain panel Mu !!!
+  port: "4003" // Ubah Jadi Port Panel Mu !!!
+};
